63c9c77b6dae901d09d14e8862dde52888399249
mipardo <miguelpardosam@gmail.com>
2026-05-05 15:35:31 +0200

    FIX: Created at changed format to datetime

--- app/core/entities.py
+++ app/core/entities.py
@@ -179,7 +179,7 @@ class Ticket(BaseModel):
     ticket_organization_id: int
     ticket_number: str
     ticket_title: str
-    ticket_created_at: int
+    ticket_created_at: datetime
     ticket_closed_at: datetime
 
     ticket_customer_firstname: Optional[str] = None
